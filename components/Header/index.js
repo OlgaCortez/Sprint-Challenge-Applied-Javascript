49703e7d93b5d0dd6cc6a2f74df5780b72ba0aae
Olga Cortez <olgalataviacortez@gmail.com>
2019-08-09 11:21:57 -0500

    header

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,34 @@
 //  </div >
 // And add it to the DOM in the .headerContainer component
 
-function Header() {}
+window.addEventListener('load', event => {
+
+
+const header = document.querySelector('.header-container');
+
+function Header(data) {
+    
+    const header = document.createElement('div');
+    const date = document.createElement('span');
+    const title = document.createElement('h1');
+    const temp = document.createElement('span');
+
+    header.classList.add('header-container');
+    date.classList.add('date');
+    title.classList.add('title');
+    temp.classList.add('temp');
+
+    date.textContent = data.date;
+    title.textContent = data.title;
+    temp.textContent = data.temp;
+
+    header.appendChild(date);
+    header.appendChild(title);
+    header.appendChild(temp);
+
+    return header;
+
+
+}
+
+})
